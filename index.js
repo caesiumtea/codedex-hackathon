6a43bdb6cff70de57ac25910480735810cd62d14
Glitch (glitch-hello-website) <none>
2023-12-13 08:04:16 +0000

    ⚾️📷 Checkpoint ./index.js:42630022/1194 ./style.css:42630022/413

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ function populateHabits() {
   let habitsDiv = document.getElementById("habits");
   for (let i = 0; i < habitTitles.length; i++) {
     let newDiv = document.createElement('div');
-    newDiv.textContent = habitTitles[i].title;
+    newDiv.classList = "home-habit";
+    let newH3 = document.createElement('h3');
+    newH3.classList = "home-habit-title";
+    newH3.textContent = habitTitles[i].title;
+    newDiv.appendChild(newH3);
+    habitsDiv.appendChild(newDiv);
   }
 }
