0769c4c12b670ba6efa74ee9a07b3eb618d87149
caesiumtea <caesiumtea@gmail.com>
2023-12-13 07:14:05 -0500

    Generate habits list from tracking instead of habitTitles

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ function startTracking() {
     tracking.days.push(newDay);
     localStorage.setItem("tracking", JSON.stringify(tracking));
   }
-  if (!localStorage.getItem("habitTitles")) {
-    localStorage.setItem("habitTitles", "[]");
-  }
+  // if (!localStorage.getItem("habitTitles")) {
+  //   localStorage.setItem("habitTitles", "[]");
+  // }
 }
 
 function populateHabits() {
@@ -19,15 +19,18 @@ function populateHabits() {
   let habitsDiv = document.getElementById("habits");
   // habitsDiv.style.backgroundColor = "white";
   // habitsDiv.style.height = "10px";
-  console.log(habitTitles.length);
-  habitTitles = JSON.parse(localStorage.getItem("habitTitles"));
-  for (let i = 0; i < habitTitles.length; i++) {
-    console.log(habitTitles[i]);
+
+  // habitTitles = JSON.parse(localStorage.getItem("habitTitles"));
+   let tracking = JSON.parse(localStorage.getItem("tracking"));
+   let latestDay = tracking.days[tracking.days.length - 1];
+   
+  for (let i = 0; i < latestDay.habits.length; i++) {
+    console.log(latestDay.habits[i]);
     let newDiv = document.createElement('div');
     newDiv.classList = "home-habit";
     let newH3 = document.createElement('h3');
     newH3.classList = "home-habit-title";
-    newH3.textContent = habitTitles[i];
+    newH3.textContent = latestDay.habits[i].title;
     newDiv.appendChild(newH3);
     habitsDiv.appendChild(newDiv);
   }
