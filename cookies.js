331450d06ccac3044433bed6dcb4fc60b2dc03f3
Glitch (glitch-hello-website) <none>
2023-12-13 06:53:18 +0000

    👼🌺 Checkpoint ./cookies.js:63604415/503 ./viewCheckboxHabit.html:63604415/85 ./viewCountingHabit.html:63604415/85 ./home.html:63604415/85 ./addHabit.html:63604415/199

---
+++ cookies.js
@@ -0,0 +1,20 @@
+function setCookie(cName, cValue) {
+  document.cookie = cName + "=" + cValue;
+}
+
+//Copied below function from w3schools
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
