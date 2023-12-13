bd9b484b1e16cd22ab9cebb0912079f75f103569
Glitch (glitch-hello-website) <none>
2023-12-13 06:24:22 +0000

    🦁🏑 Checkpoint ./script.js:63604415/2585 ./index.html:63604415/2613

--- script.js
+++ script.js
@@ -1,34 +1,16 @@
-/*
-  This is your site JavaScript code - you can add interactivity!
-*/
-
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
-console.log("Hello 🌎");
-
-/* 
-Make the "Click me!" button move when the visitor clicks it:
-- First add the button to the page by following the steps in the TODO 🚧
-*/
-const btn = document.querySelector("button"); // Get the button from the page
-if (btn) { // Detect clicks on the button
-  btn.onclick = function () {
-    // The 'dipped' class in style.css changes the appearance on click
-    btn.classList.toggle("dipped");
-  };
-}
-
-
-// ----- GLITCH STARTER PROJECT HELPER CODE -----
-
-// Open file when the link in the preview is clicked
-let goto = (file, line) => {
-  window.parent.postMessage(
-    { type: "glitch/go-to-line", payload: { filePath: file, line: line } }, "*"
-  );
-};
-// Get the file opening button from its class name
-const filer = document.querySelectorAll(".fileopener");
-filer.forEach((f) => {
-  f.onclick = () => { goto(f.dataset.file, f.dataset.line); };
+function get(){
+  gapi.client.sheets.spreadsheets.values.get({
+    spreadsheetId: '1MXQC1bXosDb6Ov9NI4YCJ-bLEOFpUyWj1r92gA65ySc',
+    range: 'Sheet1!A1:B5', // Specify the range you want to retrieve
+}).then((response) => {
+    const data = response.result.values;
+    if (data) {
+        // Process and use the retrieved data
+        console.log(data);
+    } else {
+        console.log('No data found.');
+    }
+}).catch((err) => {
+    console.error('The API returned an error:', err);
 });
+}
